e069576f96a835a68d67c78ee4cfe623dd477a45
opot <386705072@qq.com>
2025-07-09 17:27:12 +0800

    updatev3
    更新静态页面样式无法显示的问题

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'dist',
+  // GitHub Pages 配置
+  basePath: '/nextjs-web',
+  assetPrefix: '/nextjs-web/',
   images: {
     unoptimized: true
   }
